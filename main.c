46bb0f637171f136c15eddc3796cbbfc2e442d49
Blacks <mj_ol@outlook.com>
2019-07-10 12:39:48 +0800

    UPDATE:debuging core part

--- main.c
+++ main.c
@@ -26,56 +26,60 @@ typedef struct object{
 typedef struct pixel{
     COORD pos;
     struct pixel *next;
+    struct pixel *before;
 } pixel;
-//declare basic parameters
-int score = 0;
-int delta = 1;
-int state = 0;
-int delay = 200;
-int length = 3;
-//define snake and food
-struct object *snake;
-struct object *food;
-//define windows parameters
-HANDLE console;
+
 
 //declare functions
 void locate(int x,int y);
 void setColor(int c);
 void clearScreen();
-void refresh();
-void move(int *direction, COORD *pos);
-void append(struct object *o);
+void move(int *direction, struct object *s);
 void initScreen();
-void printSnake();
-*struct object initSnake(int len);
+void printSnake(struct object *s);
+struct object* initSnake(int len, COORD pos);
 //get input from keyboard
 //w -> 1  s -> 2  a -> 3  d -> 4
 int getKey(int *direction);
 
 //define functions
 //initialize a snake of certain length(default 3)
-*struct object initSnake(int len){
+struct object* initSnake(int len, COORD pos){
     struct pixel *head = NULL;
     struct pixel *p = NULL;
     struct pixel *current = NULL;
-    snake = (*struct snake)malloc(sizeof(struct object));
-    //set head and end to null
-    snake.head = NULL;
-    snake.end = NULL;
+    struct object *s = (struct object*)malloc(sizeof(struct object));
+    //set head and end to null,set pos
+    s->head = NULL;
+    s->end = NULL;
+    s->pos = pos;
     for(int i = 0;i < len; i++){
-        struct pixel *current = (*struct pixel)malloc(sizeof(struct pixel));
+        struct pixel *current = (struct pixel*)malloc(sizeof(struct pixel));
+        current->next = NULL;
+        current->before = NULL;
+        current->pos = pos;
+        pos.X += 2;
         if(head == NULL){
             head = current;
             p = head;
         }
-        p.next = current;
+        p->next = current;
+
+        current->before = p;
         if (i != (len - 1))
-            p = p.next;
+            p = p->next;
     }
-    snake.head = head;
-    snake.end = p;
-    return snake;
+    s->head = head;
+    s->end = p;
+    return s;
+}
+void printSnake(struct object *s){
+    struct pixel *p;
+    for(p = s->head;p != NULL;p = p->next){
+        COORD pos = p->pos;
+        locate(pos.X, pos.Y);
+        printf("■");
+    }
 }
 void clearScreen(){
     system("cls");
@@ -106,34 +110,28 @@ void initScreen(int size_x, int size_y){
 }
 //get input
 int getKey(int *direction){
-    if (GetAsyncKeyState(VK_A) && (direction != rightward)){
+    if (GetAsyncKeyState(VK_A) && (*direction != rightward)){
         *direction = 3;
         return 3;
     }
-    if (GetAsyncKeyState(VK_D) && (direction != leftward)){
+    if (GetAsyncKeyState(VK_D) && (*direction != leftward)){
         *direction = 4;
         return 4;
     }
-    if (GetAsyncKeyState(VK_W) && (direction != downward)){
+    if (GetAsyncKeyState(VK_W) && (*direction != downward)){
         *direction = 1;
         return 1;
     }
-    if (GetAsyncKeyState(VK_S) && (direction != upward)){
+    if (GetAsyncKeyState(VK_S) && (*direction != upward)){
         *direction = 2;
         return 2;
     }
-    //return *direction;
+    return *direction;
 }
 //move snake
 //w -> 1  s -> 2  a -> 3  d -> 4
-
-void append(int direction, struct object *s){
-
-    switch (direction):
-
-}
 void move(int *direction, struct object *s){
-    *direction = getKey(*direction);
+    *direction = getKey(direction);
     COORD pos = s->pos;
     switch (*direction){
         case 1: pos.Y--;break;
@@ -143,40 +141,65 @@ void move(int *direction, struct object *s){
         default: break;
     }
     //append head in move direction
-    struct pixel *newHead = (*struct pixel)malloc(sizeof(struct pixel));
-    newHead->pos = pos;
-    newHead.next = s->head;
-    s->head = newHead;
+    struct pixel *p = (struct pixel*)malloc(sizeof(struct pixel));
+    p->pos = pos;
+    p->next = s->head;
+    s->head = p;
+    locate(p->pos.X, p->pos.Y);
+    printf("■");
     //remove tail
+    p = s->end;
+    locate(p->pos.X,p->pos.Y);
+    printf(" ");
+    p = p->before;
+    p->next = NULL;
+    free(s->end);
+    s->end = p;
 
 }
 void test(){
 
 }
+//declare basic parameters
+int score = 0;
+int delta = 1;
+int state = 0;
+int delay = 200;
+int length = 3;
+//set initial coordinate is rows 20 cols 50
+COORD spawnPos = {50,20};
+
+//define windows parameters
+HANDLE console;
 int main()
 {
-    //set initial coordinate is rows 20 cols 50
-    COORD s = {50,20};
+    //define snake and food
+    struct object *snake = initSnake(length,spawnPos);
+    struct object *food;
     //set initial direction is left
     int direction = 3;
-
     system("chcp 65001");
     initScreen(100, 40);
     clearScreen();
-    printSnake();
-    locate(s.X,s.Y);
-    printf("■");
-    while (1){
-        move(&direction, &s);
-        locate(s.X,s.Y);
-        printf("■");
-
-
-        Sleep(500);
+    printSnake(snake);
+    locate(0,38);
+    for(struct pixel *p = snake->head;p != NULL;p = p->next){
+        printf("(%d,%d)",p->pos.X,p->pos.Y);
     }
-    printf("■ %d %d",s.X,s.Y);
-    locate(0,0);
-    printf(" ");
-    initScreen(100,40);
+    printf("end(%d,%d)",snake->end->pos.X,snake->end->pos.Y);
+    move(&direction, snake);
+
+    locate(0,39);
+    for(struct pixel *p = snake->head;p != NULL;p = p->next){
+        printf("(%d,%d)",p->pos.X,p->pos.Y);
+    }
+    printf("end(%d,%d)",snake->end->pos.X,snake->end->pos.Y);
+//    while (1){
+//
+//        printf("%d",direction);
+//        Sleep(500);
+//
+//    }
+
     return 0;
 }
